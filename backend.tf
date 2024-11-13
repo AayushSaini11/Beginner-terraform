19d6e926a92aabf45a7d65164a555ca38bb44e11
AayushSaini11 <153816894+AayushSaini11@users.noreply.github.com>
2024-11-13 13:00:16 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "terraform-state-bucket-new009"
+    bucket = "terraform-state-bucket-new007"
     key     = "terraform.tfstate"
     region  = "us-west-2"
     encrypt = true
