ef283d89800883bfde20e119679e9dd138118c03
AayushSaini11 <153816894+AayushSaini11@users.noreply.github.com>
2024-11-13 12:46:15 +0530

    changed bucket name

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "your-terraform-state-bucket"
+    bucket = "terraform-state-bucket-new009"
     key     = "terraform.tfstate"
     region  = "us-west-2"
     encrypt = true
